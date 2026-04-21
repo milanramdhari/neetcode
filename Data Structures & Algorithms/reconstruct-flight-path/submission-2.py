bf0e518fe4e35dc1b22e1cb985bd74daae2dc7ce
milanramdhari <147016871+milanramdhari@users.noreply.github.com>
2026-04-21 03:21:29 -0400

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        path = []
+
+        if not tickets:
+             return path
+        
+        graph = {}
+        for depart, arrival in tickets:
+            if depart not in graph:
+                graph[depart] = []
+            if arrival not in graph:
+                graph[arrival] = []
+            
+            graph[depart].append(arrival)
+
+        for arr in graph.values():
+            arr.sort()
+
+        def dfs(start):
+            # if len(graph[start]) == 0:
+            #     path.insert(0, start)
+            #     return
+            
+            while graph[start]:
+                next_stop = graph[start].pop(0)
+                dfs(next_stop)
+            # for neig in graph[start]:
+            #     graph[start].pop(0)
+            #     dfs(neig)
+            
+            path.insert(0, start)
+        
+        dfs("JFK")
+        return path
