68757f7cc45f7403df193a61406f91059a94d91b
Milan <147016871+milanramdhari@users.noreply.github.com>
2026-05-25 19:53:00 -0400

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # start with highest gas
+        n = len(gas)
+        surplus = [gas[i] - cost[i] for i in range(n)]
+
+        if sum(surplus) < 0:
+            return -1
+
+        for i in range(n):
+            total = surplus[i]
+            j = (i + 1) % n
+
+            while total >= 0 and j != i:
+                total += surplus[j]
+                j = (j + 1) % n
+
+            if j == i and total >= 0:
+                return i
+        
+        return -1
+
+
+            
+
+
+
+        
